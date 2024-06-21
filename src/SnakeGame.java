7636f85b87f0ec15e529c270cdd69065ecd041ce
SonniSonni <brandonlschaen@gmail.com>
2024-06-21 14:34:55 -0400

    Drew snakehead to the screen

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -5,8 +5,20 @@ import java.util.Random;
 import javax.swing.*;
 
 public class SnakeGame extends JPanel {
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     int boardWidth;
     int boardHeight;
+    int tileSize = 25;
+
+    Tile snakeHead;
 
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
@@ -14,5 +26,18 @@ public class SnakeGame extends JPanel {
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.black);
 
+        snakeHead = new Tile(5, 5);
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        //snake
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x, snakeHead.y, tileSize, tileSize);
     }
 }
